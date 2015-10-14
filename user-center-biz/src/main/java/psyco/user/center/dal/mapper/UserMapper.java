79c671717a2682483ce51524fe4a62276849a181
lipeng <dianxiangan@aliyun.com>
2015-10-14 23:34:55 +0800

    generated code

---
+++ user-center-biz/src/main/java/psyco/user/center/dal/mapper/UserMapper.java
@@ -0,0 +1,18 @@
+package psyco.user.center.dal.mapper;
+
+import java.util.List;
+import psyco.user.center.dal.entity.User;
+
+public interface UserMapper{
+
+    public User findOne(Long id);
+
+    public List<User> find(List<Long> ids);
+
+    public void insert(User user);
+
+    public void update(User user);
+
+    public void delete(Long id);
+
+}
